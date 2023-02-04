b793d20f8e61fb9ca2da51710dbb414337e702c2
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-02-04 16:08:17 +0300

    updated AppRouter component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom"
+import Footer from "../components/Footer"
+import SearcBar from "../components/SearcBar"
 import Home from "../pages/Home"
 
 const AppRouter = () => {
   return (
     <>
+        <SearcBar />
         <Routes>
             <Route path="/" element={<Home />}/>
         </Routes>
+        <Footer />
     </>
   )
 }
