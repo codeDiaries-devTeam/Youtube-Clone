a0c67d858ff07d2d8851fc608c935c79a2487514
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-02-04 16:08:48 +0300

    created redux structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './redux/app/store';
 import AppRouter from './router/AppRouter';
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <AppRouter />
-    </div>
+    </Provider>
   );
 }
 
